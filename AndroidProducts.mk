aeee0f1447074b6dec09ea0040fb7c164f592193
Jose Adrian <laptopboytoshiba3@gmail.com>
2026-07-13 05:40:24 +0200

    Initial device tree

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,6 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_a14m.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_a14m-user \
-    twrp_a14m-userdebug \
-    twrp_a14m-eng
+    twrp_a145r-user \
+    twrp_a145r-userdebug \
+     twrp_a145r-eng
